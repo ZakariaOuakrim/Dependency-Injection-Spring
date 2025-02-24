8eba46dca282609ced40e12e9b7a0e361f56916c
ZakariaOuakrim <zakarialukyguy@gmail.com>
2025-02-24 10:16:47 +0000

    injection via constructor

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -4,6 +4,11 @@ import dao.IDao;
 
 public class MetierImpl implements IMetier {
     private IDao dao;
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
     @Override
     public double calcul() {
         double t = dao.getData();
--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -6,8 +6,8 @@ import metier.MetierImpl;
 public class PresentationV1 {
     public static void main(String[] args) {
         DaoImpl d = new DaoImpl();
-        MetierImpl metier = new MetierImpl();
-        
-        System.out.println();
+        MetierImpl metier = new MetierImpl(d);
+        //metier.setDao(d); //injection via le setter
+        System.out.println("Resultat "+ metier.calcul());
     }
 }
